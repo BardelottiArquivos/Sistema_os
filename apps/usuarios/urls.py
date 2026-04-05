7b83a8f1424e183b378230b60c0fadf70c3927ff
Carlos Bardelorri <carlosbardelotti@gmail.com>
2026-04-04 22:02:32 -0300

    Atualiza arquivos

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    
+    # CRUD de usuários
+    path('lista/', views.UsuarioListView.as_view(), name='usuario_list'),
+    path('novo/', views.UsuarioCreateView.as_view(), name='usuario_create'),
+    path('<int:pk>/editar/', views.UsuarioUpdateView.as_view(), name='usuario_update'),
+    path('<int:pk>/excluir/', views.UsuarioDeleteView.as_view(), name='usuario_delete'),
 ]
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -6,6 +6,62 @@ from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from apps.ordens_servico.models import OrdemServico
 
+# ----------------------------------------------------------------------------
+# Esta sendo adcionado para criar usuário
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Usuario
+
+class UsuarioListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Usuario
+    template_name = 'usuarios/usuario_list.html'
+    context_object_name = 'usuarios'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return Usuario.objects.all().order_by('-date_joined')
+    
+    def test_func(self):
+        return self.request.user.tipo == 'admin' or self.request.user.is_superuser
+
+
+class UsuarioCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Usuario
+    template_name = 'usuarios/usuario_form.html'
+    fields = ['username', 'first_name', 'last_name', 'email', 'tipo', 'telefone', 'matricula', 'ativo']
+    success_url = reverse_lazy('usuario_list')
+    
+    def test_func(self):
+        return self.request.user.tipo == 'admin' or self.request.user.is_superuser
+    
+    def form_valid(self, form):
+        form.instance.set_password('123456')
+        return super().form_valid(form)
+
+
+class UsuarioUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Usuario
+    template_name = 'usuarios/usuario_form.html'
+    fields = ['first_name', 'last_name', 'email', 'tipo', 'telefone', 'matricula', 'ativo']
+    success_url = reverse_lazy('usuario_list')
+    
+    def test_func(self):
+        return self.request.user.tipo == 'admin' or self.request.user.is_superuser
+
+
+class UsuarioDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Usuario
+    template_name = 'usuarios/usuario_confirm_delete.html'
+    success_url = reverse_lazy('usuario_list')
+    
+    def test_func(self):
+        return self.request.user.tipo == 'admin' or self.request.user.is_superuser
+    
+    def get_queryset(self):
+        return Usuario.objects.exclude(id=self.request.user.id)
+
+# -----------------------------------------------------------
 
 def login_view(request):
     if request.method == 'POST':
