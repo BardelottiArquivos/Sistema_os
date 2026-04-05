cce128d673c5f77e8ec31204a0495ebd08f592fc
Carlos Bardelorri <carlosbardelotti@gmail.com>
2026-04-05 02:43:04 -0300

    Atualização

--- apps/ordens_servico/views.py
+++ apps/ordens_servico/views.py
@@ -1,30 +1,107 @@
+# from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+# from django.urls import reverse_lazy
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from .models import OrdemServico
+
+# class OSListView(LoginRequiredMixin, ListView):
+#     model = OrdemServico
+#     template_name = 'ordens_servico/os_list.html'
+#     context_object_name = 'ordens'
+#     paginate_by = 10
+
+# class OSCreateView(LoginRequiredMixin, CreateView):
+#     model = OrdemServico
+#     template_name = 'ordens_servico/os_form.html'
+#     fields = '__all__'
+#     success_url = reverse_lazy('os_list')
+
+# class OSDetailView(LoginRequiredMixin, DetailView):
+#     model = OrdemServico
+#     template_name = 'ordens_servico/os_detail.html'
+#     context_object_name = 'os'
+
+# class OSUpdateView(LoginRequiredMixin, UpdateView):
+#     model = OrdemServico
+#     template_name = 'ordens_servico/os_form.html'
+#     fields = '__all__'
+#     success_url = reverse_lazy('os_list')
+
+# class OSDeleteView(LoginRequiredMixin, DeleteView):
+#     model = OrdemServico
+#     template_name = 'ordens_servico/os_confirm_delete.html'
+#     success_url = reverse_lazy('os_list')
+
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib import messages
 from .models import OrdemServico
+from apps.usuarios.models import Usuario
+
 
 class OSListView(LoginRequiredMixin, ListView):
     model = OrdemServico
     template_name = 'ordens_servico/os_list.html'
     context_object_name = 'ordens'
     paginate_by = 10
+    
+    def get_queryset(self):
+        user = self.request.user
+        queryset = OrdemServico.objects.all()
+        
+        # Técnico vê apenas suas OS
+        if user.tipo == 'tecnico' and not user.is_superuser:
+            queryset = queryset.filter(tecnico_responsavel=user)
+        
+        return queryset.order_by('-data_abertura')
+
 
 class OSCreateView(LoginRequiredMixin, CreateView):
     model = OrdemServico
     template_name = 'ordens_servico/os_form.html'
-    fields = '__all__'
+    fields = ['cliente', 'computador', 'tecnico_responsavel', 'titulo', 
+              'descricao_problema', 'observacoes', 'prioridade', 'data_previsao', 
+              'valor_mao_obra', 'valor_pecas', 'desconto']
     success_url = reverse_lazy('os_list')
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        # Mostrar apenas usuários do tipo técnico
+        form.fields['tecnico_responsavel'].queryset = Usuario.objects.filter(tipo='tecnico')
+        form.fields['computador'].required = False
+        return form
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Ordem de Serviço criada com sucesso!')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Erro ao criar Ordem de Serviço. Verifique os dados.')
+        return super().form_invalid(form)
+
 
 class OSDetailView(LoginRequiredMixin, DetailView):
     model = OrdemServico
     template_name = 'ordens_servico/os_detail.html'
     context_object_name = 'os'
 
+
 class OSUpdateView(LoginRequiredMixin, UpdateView):
     model = OrdemServico
     template_name = 'ordens_servico/os_form.html'
-    fields = '__all__'
+    fields = ['cliente', 'computador', 'tecnico_responsavel', 'titulo', 
+              'descricao_problema', 'observacoes', 'solucao_aplicada',
+              'prioridade', 'status', 'data_previsao', 'data_inicio', 'data_conclusao',
+              'valor_mao_obra', 'valor_pecas', 'desconto']
     success_url = reverse_lazy('os_list')
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['tecnico_responsavel'].queryset = Usuario.objects.filter(tipo='tecnico')
+        form.fields['computador'].required = False
+        return form
+
 
 class OSDeleteView(LoginRequiredMixin, DeleteView):
     model = OrdemServico
